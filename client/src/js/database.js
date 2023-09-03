1323aae8b872209adf92eaf4a86ae0b86b4a1ed5
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-09-02 19:59:40 -0500

    fixed js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,34 +2,34 @@ import { openDB } from 'idb';
 
 const initdb = async () =>
 // We are creating a new database named 'contact' which will be using version 1 of the database.
-  openDB('contact', 1, {
+  openDB('textEditor', 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('contact')) {
-        console.log('contact database already exists');
+      if (db.objectStoreNames.contains('textEditor')) {
+        console.log('textEditor database already exists');
         return;
       }
       // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-      db.createObjectStore('contact', { keyPath: 'id', autoIncrement: true });
-      console.log('contact database created');
+      db.createObjectStore('textEditor', { keyPath: 'id', autoIncrement: true });
+      console.log('textEditor database created');
     },
   });
 
 // Export a function we will use to POST to the database.
-export const postDb = async (name, home, cell, email)  => {
-  console.log('Post to the database');
+export const putDb = async (content)  => {
+  console.log('Put to the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const textEditorDb = await openDB('textEditor', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = textEditorDb.transaction('textEditor', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('textEditor');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const request = store.put({ id: 1, value: content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -42,16 +42,16 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('textEditor', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = contactDb.transaction('textEditor', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('textEditor');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
@@ -59,28 +59,6 @@ export const getDb = async () => {
   return result;
 };
 
-
-// Export a function we will use to DELETE to the database.
-export const deleteDb = async (id) => {
-  console.log('DELETE from the database', id);
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .delete() method to get all data in the database.
-  const request = store.delete(id);
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result?.value;
-};
 
 // Start the database.
 initdb();
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,10 +1,33 @@
 import { Workbox } from 'workbox-window';
-import '../css/styles.css';
+import '../css/style.css';
+import Editor from './editor';
+import './database';
+
+const main = document.querySelector('#main');
+main.innerHTML = '';
+
+
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // Register workbox service worker
-  const workboxSW = new Workbox('./src-sw.js');
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
